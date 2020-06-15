53602c5965924387c817e71ba00b5d1bdcb26ea3
Fahad Ali <itsfahadmughal>
2020-06-15 22:17:14 +0500

    Navigation Drawer Add

---
+++ app/src/main/java/com/example/lynxdispatch/DispatchHomeActivity.java
@@ -0,0 +1,141 @@
+package com.example.lynxdispatch;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class DispatchHomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private boolean doubleBackToExitPressedOnce = false;
+    private TextView username, car;
+    private ImageView userprofile;
+    private SharedPreferences sharedpreferences;
+    private SharedPreferences.Editor editor;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dispatch_home);
+
+        initialization();
+
+    }
+
+    private void initialization() {
+        toolbar = findViewById(R.id.toolbar_dispatcher);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(DispatchHomeActivity.this, drawerLayout, toolbar, R.string.navigation_open, R.string.navigation_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View header_view = navigationView.getHeaderView(0);
+        username = header_view.findViewById(R.id.nav_name_user);
+        car = header_view.findViewById(R.id.nav_name_user);
+        userprofile = header_view.findViewById(R.id.navProfile);
+        navigationView.setNavigationItemSelectedListener(DispatchHomeActivity.this);
+
+        sharedpreferences = getSharedPreferences("login_data", MODE_PRIVATE);
+        editor = sharedpreferences.edit();
+        String email = sharedpreferences.getString("email", "");
+        username.setText(email);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.dispatch_item1:
+                drawerLayout.closeDrawer(Gravity.LEFT);
+                break;
+            case R.id.dispatch_item2:
+                Toast.makeText(this, "Assigned Trips", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dispatch_item3:
+                Toast.makeText(this, "Trip History", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dispatch_item4:
+                Toast.makeText(this, "Earnings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dispatch_item5:
+                Toast.makeText(this, "Helping Material", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dispatch_item6:
+                Toast.makeText(this, "Vehicle Expense", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dispatch_item7:
+                Toast.makeText(this, "Support", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dispatch_item8:
+                Toast.makeText(this, "Payment", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dispatch_item9:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dispatch_item10:
+                Intent intent = new Intent(DispatchHomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+                editor.putInt("flagLogin", 0);
+                editor.putString("email", "");
+                editor.putString("password", "");
+                editor.apply();
+                break;
+        }
+
+        return false;
+    }
+
+
+    public void onBackPressed() {
+
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        if (drawerLayout.isShown() == true) {
+            drawerLayout.closeDrawer(Gravity.LEFT);
+            this.doubleBackToExitPressedOnce = false;
+        }
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+        this.doubleBackToExitPressedOnce = true;
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
+
+    }//end of back pressed
+}
--- app/src/main/java/com/example/lynxdispatch/LoginActivity.java
+++ app/src/main/java/com/example/lynxdispatch/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.lynxdispatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -11,12 +13,16 @@ import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.sql.Driver;
+
 public class LoginActivity extends AppCompatActivity {
 
 
     private TextView forgetPassword, newAccount;
     private Button loginButton;
     private TextInputEditText email_e, password_e;
+    private SharedPreferences sharedpreferences;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,15 +57,35 @@ public class LoginActivity extends AppCompatActivity {
     private void loginFunc() {
         String s = email_e.getText().toString().trim();
         String s1 = password_e.getText().toString();
-        String emailValidation="[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-        if (TextUtils.isEmpty(s)||!s.matches(emailValidation)) {
+        String emailValidation = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        if (TextUtils.isEmpty(s) || !s.matches(emailValidation)) {
             email_e.setError("Invalid email format");
         }
         if (TextUtils.isEmpty(s1)) {
             Toast.makeText(this, "Password: This field must be filled!", Toast.LENGTH_LONG).show();
         } else {
-            Toast.makeText(this, "Email : " + s + "\nPassword : " + s1, Toast.LENGTH_SHORT).show();
-        }
+            if (s1.equals("123")) {
+                Intent intent = new Intent(LoginActivity.this, DispatchHomeActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+
+                editor.putInt("flagLogin", 1);
+                editor.putString("email", s);
+                editor.putString("password", s1);
+                editor.apply();
+            } else if (s1.equals("1234")) {
+                Intent intent = new Intent(LoginActivity.this, DriverHomeActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+
+                editor.putInt("flagLogin", 2);
+                editor.putString("email", s);
+                editor.putString("password", s1);
+                editor.apply();
+            }
+        }//else of checking empty fields in login username password
     }
 
     private void initialization() {
@@ -68,5 +94,23 @@ public class LoginActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.button);
         email_e = findViewById(R.id.email_login);
         password_e = findViewById(R.id.password_login);
+
+
+        sharedpreferences = getSharedPreferences("login_data", MODE_PRIVATE);
+        editor = sharedpreferences.edit();
+
+        int loginflag = sharedpreferences.getInt("flagLogin", 0);
+
+        if (loginflag == 1) {
+            Intent intent = new Intent(LoginActivity.this, DispatchHomeActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+        } else if (loginflag == 2) {
+            Intent intent = new Intent(LoginActivity.this, DriverHomeActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+        }
     }
 }
