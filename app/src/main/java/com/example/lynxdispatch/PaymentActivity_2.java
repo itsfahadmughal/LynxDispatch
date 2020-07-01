2dca877e6f7e2b80713c0443a53d8fde6f764052
Fahad Ali <itsfahadmughal>
2020-07-02 01:46:53 +0500

    Revise Bank Payment Activity

--- app/src/main/java/com/example/lynxdispatch/PaymentActivity_2.java
+++ app/src/main/java/com/example/lynxdispatch/PaymentActivity_2.java
@@ -44,11 +44,11 @@ public class PaymentActivity_2 extends AppCompatActivity {
         String s2 = RoutingNo.getText().toString().trim();
 
         if (TextUtils.isEmpty(s2)) {
-            AccountNo.setError("Account #: This field must be filled!");
-        } else if (TextUtils.isEmpty(s)) {
-            ConfirmAccountNo.setError("Confirm Account #: This field must be filled!");
-        } else if (TextUtils.isEmpty(s1)) {
             RoutingNo.setError("Routing #: This field must be filled!");
+        } else if (TextUtils.isEmpty(s)) {
+            AccountNo.setError("Account #: This field must be filled!");
+        } else if (TextUtils.isEmpty(s1)) {
+            ConfirmAccountNo.setError("Confirm Account #: This field must be filled!");
         } else {
             if (s.equals(s1)) {
                 Toast.makeText(this, "Dummy Saved!!!" + s2 + " " + s1 + " " + s, Toast.LENGTH_SHORT).show();
