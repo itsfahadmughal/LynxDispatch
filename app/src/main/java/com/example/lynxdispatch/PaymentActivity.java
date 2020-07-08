3d1c5959a04250989223ecd36edcd8c6cd0cf89f
Fahad Ali <itsfahadmughal>
2020-07-09 00:15:12 +0500

    Add Credit Card Detail

--- app/src/main/java/com/example/lynxdispatch/PaymentActivity.java
+++ app/src/main/java/com/example/lynxdispatch/PaymentActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 
 public class PaymentActivity extends AppCompatActivity {
 
-    private Button addbankAccount, backButton;
+    private Button addbankAccount, backButton, addcreditCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +35,25 @@ public class PaymentActivity extends AppCompatActivity {
                 overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
             }
         });
+
+        addcreditCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PaymentActivity.this, PaymentActivity_3.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+            }
+        });
+
+
     }
 
     private void inialization() {
         addbankAccount = findViewById(R.id.button_payment);
+        addcreditCard = findViewById(R.id.button_payment2);
         backButton = findViewById(R.id.backButton_go_to_payment);
         addbankAccount.setBackgroundColor(Color.WHITE);
+        addcreditCard.setBackgroundColor(Color.WHITE);
 
     }
 
---
+++ app/src/main/java/com/example/lynxdispatch/PaymentActivity_3.java
@@ -0,0 +1,27 @@
+package com.example.lynxdispatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentActivity_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_3);
+
+        inialization();
+    }
+
+    private void inialization() {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+    }
+}
