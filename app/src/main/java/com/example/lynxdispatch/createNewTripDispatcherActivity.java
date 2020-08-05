d6231a02e73056e2514bc5e235de9036909c0fac
Fahad Ali <itsfahadmughal>
2020-08-05 20:16:35 +0500

    add dashboard design

---
+++ app/src/main/java/com/example/lynxdispatch/createNewTripDispatcherActivity.java
@@ -0,0 +1,18 @@
+package com.example.lynxdispatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createNewTripDispatcherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_trip_dispatcher);
+
+
+
+
+    }
+}
