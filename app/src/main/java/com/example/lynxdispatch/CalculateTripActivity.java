ef27f1e934c5aebf63d907576ef60af6b1f092a2
unknown <talhasahi86@gmail.com>
2020-08-10 08:54:27 -0700

    Workkig On Save Trip

--- app/src/main/java/com/example/lynxdispatch/CalculateTripActivity.java
+++ app/src/main/java/com/example/lynxdispatch/CalculateTripActivity.java
@@ -1,15 +1,69 @@
 package com.example.lynxdispatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.Date;
+import java.util.List;
 
 public class CalculateTripActivity extends AppCompatActivity {
-
+    private TextView marqueeText,nodata_save;
+    private Button newTripEstimatebtn,backButton;
+    private MaterialCalendarView calendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate_trip);
+        initialization();
+        marqueeText.setSelected(true);
+        newTripEstimatebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CalculateTripActivity.this, NewTripEstimate.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+            }
+        });
+
+
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Toast.makeText(CalculateTripActivity.this, date.toString(), Toast.LENGTH_SHORT).show();
+               Integer d = date.getDate().getDate();
+               int m = date.getMonth();
+               int y = date.getYear();
+          
+            }
+        });
+
+    }
+
+    private void initialization() {
+        calendarView = findViewById(R.id.calendarView);
+        marqueeText = findViewById(R.id.text);
+        newTripEstimatebtn = findViewById(R.id.newTripEstimatebtn);
+        backButton = findViewById(R.id.backButton_calculater);
+        nodata_save = findViewById(R.id.nodata_save);
     }
 
     @Override
--- app/src/main/java/com/example/lynxdispatch/NewTripEstimate.java
+++ app/src/main/java/com/example/lynxdispatch/NewTripEstimate.java
@@ -2,13 +2,109 @@ package com.example.lynxdispatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import me.himanshusoni.quantityview.QuantityView;
 
 public class NewTripEstimate extends AppCompatActivity {
+    private ImageView fareParameter;
+    private Button backButton,createUrgentTrip;
+    private EditText baseLocation,pickUpAddress,dropoFFAddress;
+    private  TextView baseFareValue,perMileValue,perMinVale;
+
+
+    SharedPreferences prefs;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_trip_estimate);
+        initialization();
+        String     baselocation = prefs.getString("baselocation", null);
+        String    basefare = prefs.getString("basefare", null);
+        String    permnute = prefs.getString("permnute", null);
+        String    permile = prefs.getString("permile", null);
+
+        baseFareValue.setText(basefare);
+        perMileValue.setText(permile);
+        perMinVale.setText(permnute);
+        baseLocation.setText(baselocation);
+
+        fareParameter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NewTripEstimate.this, FareParameters.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+            }
+        });
+        createUrgentTrip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean fieldsOK = validate(new EditText[]{baseLocation, pickUpAddress, dropoFFAddress});
+                if (fieldsOK) {
+                    Intent intent = new Intent(NewTripEstimate.this, UrgentTripDetail.class);
+                    startActivity(intent);
+                    overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+
+                }
+                else {
+                }
+
+
+
+            }
+        });
     }
+
+
+    private void initialization() {
+        baseFareValue =findViewById(R.id.base_fare_Value);
+        perMileValue = findViewById(R.id.per_mile_value);
+        perMinVale = findViewById(R.id.per_minute_value);
+        baseLocation = findViewById(R.id.base_location_newTrip);
+        pickUpAddress = findViewById(R.id.pickUp_Time_new_Trip);
+        dropoFFAddress = findViewById(R.id.dropoff_address_new_trip);
+        createUrgentTrip = findViewById(R.id.create_urgent_Trips);
+        backButton = findViewById(R.id.backButton_new_trip_estimate);
+        fareParameter = findViewById(R.id.fare_parameter);
+        prefs = this.getSharedPreferences("fare_parameter_data", Context.MODE_PRIVATE);
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+
+    }
+    private boolean validate(EditText[] fields) {
+        for (int i = 0; i < fields.length; i++) {
+            EditText currentField = fields[i];
+            if (currentField.getText().toString().length() <= 0) {
+                currentField.setError("Do not leave empty");
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
